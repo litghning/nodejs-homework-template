b7b58f5429ef292d1820d9c6b422b5749f89c6bc
Vladislav Plisko <103947549+litghning@users.noreply.github.com>
2023-03-26 10:53:22 +0300

    fix put

--- middlewars/contacstM.js
+++ middlewars/contacstM.js
@@ -1,23 +1,29 @@
 const Joi = require("joi");
 
 const contactSchema = Joi.object({
-  name: Joi.string().required(),
+  name: Joi.string().required().messages({
+    'any.required': `missing required name field`
+  }),
   email: Joi.string()
     .email({
       minDomainSegments: 1,
       tlds: { allow: ["com", "net"] },
     })
-    .required(),
+    .required().messages({
+      'any.required': `missing required name field`
+    }),
   phone: Joi.string()
     .regex(/^[0-9]{10}$/)
-    .required(),
+    .required().messages({
+      'any.required': `missing required name field`
+    }),
 });
 
 const validateContact = async (req, res, next) => {
     const { error,value } = contactSchema.validate(req.body);
     if (error) {
         console.log(error.label)
-      res.status(400).json({ message: "missing required name field" });
+      res.status(400).json({ message: error.details[0].message });
       return;
     }
     req.body = value;
